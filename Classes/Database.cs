2a075f533b9b46d3652c8ccf88c6dbe514261c76
Martijn Beenker <martijn.beenker@gmail.com>
2018-06-17 09:17:37 +0200

    Eerste aanzet voor de database class

---
+++ Classes/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Database
+    {
+        private DataSource databaseDs;
+        private List<Table> databaseTables;
+
+        public Database(string dacpacPath)
+        {
+            databaseDs = null;
+            databaseTables = null;
+        }       
+    }
+}
