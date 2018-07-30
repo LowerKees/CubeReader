3a512f5ca2b384dde845afbba3cec7b78f6c81d1
Martijn Beenker <martijn.beenker@gmail.com>
2018-07-30 09:54:17 +0200

    Toevoegen van MatchException class voor het throwen van specifieke matching exceptions

---
+++ Classes/MatchException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class MatchException : System.Exception
+    {
+        public MatchException() { }
+        public MatchException(string message) : base(message) { }
+        public MatchException(string message, Exception innerException) : base(message, innerException){ }
+    }
+}
