71ca50168723aa85941d2cbd18fe593dd7924f37
Martijn Beenker <martijn.beenker@gmail.com>
2018-08-08 10:55:31 +0200

    Toepassen van IEqualityComparer interface voor kolommen

--- Classes/Column.cs
+++ Classes/Column.cs
@@ -6,17 +6,13 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
-    public class Column
+    public class Column : IEqualityComparer<Column>
     {
-        private string columnName, dataType;
-        private int dataTypePrec, dataTypeScale;
+        private string columnName;
 
         public Column(string columnName = null, string dataType = null, int Precision = 0, int Scale = 0)
         {
             this.columnName = columnName;
-            this.dataType = dataType;
-            this.dataTypePrec = Precision;
-            this.dataTypeScale = Scale;
         }
 
         public string _ColumnName
@@ -25,22 +21,21 @@ namespace Classes
             set { columnName = value; }
         }
 
-        public string myDataType
+        // Implementation of IEqualityComparer interface
+        bool IEqualityComparer<Column>.Equals(Column x, Column y)
         {
-            get { return dataType; }
-            set { dataType = value; }
-        }
-        
-        public int myDataTypePrec
-        {
-            get { return dataTypePrec; }
-            set { dataTypePrec = value; }
+            if(x == null || y == null)
+                return false;
+
+            if (x.columnName == y.columnName)
+                return true;
+            else
+                return false;
         }
 
-        public int myDataTypeScale
+        int IEqualityComparer<Column>.GetHashCode(Column obj)
         {
-            get { return dataTypeScale; }
-            set { dataTypeScale = value; }
+            return obj.columnName.ToLower().GetHashCode();
         }
     }
 }
