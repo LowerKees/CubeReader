478d7a3c352d5bcc7e696760608b4235006be1a5
Martijn Beenker <martijn.beenker@gmail.com>
2018-08-18 17:42:50 +0200

    Toevoegen van testproject

---
+++ HiveStackIntegrator/MatchingTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HiveStackTester
+{
+    [TestClass]
+    public class MatchingTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+        }
+    }
+}
---
+++ HiveStackIntegrator/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HiveStackIntegrator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HiveStackIntegrator")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ae4b9504-15f3-490a-869f-0f6a54383399")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
