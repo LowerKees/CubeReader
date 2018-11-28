7cede8dcb484844b6c2cb2e298c671793dda82c3
martijn.beenker@bngbank.nl <martijn.beenker@gmail.com>
2018-11-28 09:44:01 +0100

    Opruimen van program.cs class zodat hier alleen nog een logische flow ontstaat.

--- CubeReader/Program.cs
+++ CubeReader/Program.cs
@@ -16,103 +16,24 @@ namespace CubeReader
 			Dictionary<PathType, string> paths = ProgramFlow.UserInput();
 
 			// Find cubes and databases
-			List<string> xmlaFileList = GetFileList(paths[PathType.cube], "*.xmla");
-			List<string> dacpacFileList = GetFileList(paths[PathType.dacpac], "*.dacpac");
+			List<string> xmlaFileList = ProgramFlow.GetFileList(paths[PathType.cube], "*.xmla");
+			List<string> dacpacFileList = ProgramFlow.GetFileList(paths[PathType.dacpac], "*.dacpac");
 
 			// Extract databases and cubes into memory
 			List<Database> databases = UnpackDacpac.UnpackDacpacs(dacpacFileList);
 			List<Cube> cubes = UnpackCube.UnpackCubes(xmlaFileList);
 
 			// Match cubes and databases in list of matches
-			List<Match> matches = new List<Match>();
-			try
-			{
-				foreach (Cube cube in compareCubeList)
-				{
-					matches.Add(Match.MatchCubeToDatabase(databases, cube));
-				}
-			}
-			catch (MatchException me)
-			{
-				Console.WriteLine(me.Message);
-			}
-			catch (Exception e)
-			{
-				throw;
-			}
+			List<Match> matches = Match.CreateMatchList(databases, cubes);
 
 			// Run matching checks
-			try
-			{
-				foreach (Match match in matches)
-				{
-					// Print out the information 
-					IntroduceCubeChecks(match.MatchingCube._cubeName);
-					// Check if all cube tables are
-					// matched in the database
-					Check.CheckForTables(match);
-					// Check if all cube columns have
-					// corresponding database columns
-					// and that the data type is a match
-					Check.CheckForColumns(match);
-				}
-			}
-			catch (MatchException me)
-			{
-				Console.ForegroundColor = ConsoleColor.DarkRed;
-				Console.WriteLine();
-				Console.WriteLine($"ERROR: {me.Message}");
-				Console.WriteLine("An error occured: execution aborted.");
-				Console.ResetColor();
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e.Message);
-				Console.WriteLine(e.StackTrace);
-			}
+			Check.RunChecks(matches);
 
-			// Run additional info statements
-			try
-			{
-				Console.WriteLine("\n*******************************************");
-				Console.WriteLine("***** Running additional info statements\n");
-
-				foreach (Cube cube in compareCubeList)
-				{
-					Information.OutputLogicalColumns(cube);
-				}
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e.Message);
-				Console.WriteLine(e.StackTrace);
-			}
+			// Run additional info checks
+			Check.RunAdditionalCubeChecks(cubes);
 
 			// TODO: remove debug statement
 			Console.ReadKey();
-		}
-
-
-
-		private static List<string> GetFileList(string path, string extension)
-		{
-			List<string> xmlaFiles = new List<string>();
-			xmlaFiles.AddRange(Directory.GetFiles(path, extension));
-
-			foreach (string xmlaFile in xmlaFiles)
-			{
-				Console.WriteLine($"Found the file {xmlaFile}");
-			}
-
-			return xmlaFiles;
-		}
-
-
-
-		private static void IntroduceCubeChecks(string cubeName)
-		{
-			Console.WriteLine("\n*****************************************************");
-			Console.WriteLine($"***** Running checks for {cubeName}...\n");
 		}
 	}
 }
